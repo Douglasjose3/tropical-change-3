89f11a3f6ecf875aed207d6add8afd1dff555998
Douglas Rodrigues José <douglasjose3@gmail.com>
2024-06-08 14:20:44 -0700

    Faltando resolver o scroll.

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -10,19 +10,18 @@ const About = () => {
     }, [])
     return (
         <>
-            <div id="about" className="bg-about bg-cover bg-fixed min-w-screen min-h-screen hover:sm: bg-center">
+            <div id="about" className="bg-about bg-cover bg-fixed max-w-full h-screen hover:sm: bg-center">
                 <div className="flex justify-center items-center">
-                        <div className="lg:pt-[100px] lg:ml-10 hover:sm: pt-[50px]">
-                            <div className="">
-                                <h1 className="font-black lg:text-[70px] text-center lg:text-black mb-[-30px] hover:sm: text-[50px]" data-aos = 'fade-right'>Tropical</h1>
-                                <h1 className="font-black lg:text-[70px] text-center lg:text-black hover:sm: text-[50px]" data-aos = 'fade-left'>Change</h1>
+                        <div className="pt-[110px]">
+                            <div className="textH1 hover:sm: text-[55px] hover:sm:text-wrap">
+                                <h1 className="" data-aos = 'fade-right'>Tropical Change</h1>
                             </div>
-                            <div className="text-center font-bold text-2xl rounded-3xl lg:text-black lg:w-[550px] p-5 backdrop-brightness-150 hover:sm: w-full">
+                            <div className="aboutP hover:sm: max-w-sm">
                                 <p className="">"We want to make a difference in the Day Trading Community, by removing stress and adding confidence to your trades".</p>
                                 <p>We do not intend to sell anything. We are a group of traders who like to share how we navigate the market.</p>
                             </div>
                             <div className='flex justify-center animate-pulse'>
-                                <button className="flex items-center border-2 px-4 py-4 rounded-xl mt-8 bg-blueMedium text-white shadow-lg uppercase font-bold text-nowrap hover:bg-orangeLight hover:text-orangeDark hover:border-orangeDark"
+                                <button className="aboutButton"
                                     type="button">
                                     <p className="text-[30px] mr-3">
                                         <AiFillDiscord/>
--- src/Component/Whats.jsx
+++ src/Component/Whats.jsx
@@ -15,9 +15,9 @@ const Whats = () => {
         <div id="whats" className="bg-cover bg-chart bg-fixed min-w-screen min-h-screen hover:sm: bg-center">
             <div className="">
                 <div className="text-white p-10 backdrop-blur-md rounded-xl pt-[120px]">
-                    <div className="">
-                        <h1 className="font-black lg:text-[70px] text-center text-white mb-[-30px] hover:sm: text-[50px]" data-aos = 'fade-right'>What's</h1>
-                        <h1 className="font-black lg:text-[70px] text-center text-white mb-1 hover:sm: text-[50px]" data-aos = 'fade-left'>Included</h1>
+                    <div className="textH1 hover:sm: text-[50px]">
+                        <h1 className="mb-[-30px]" data-aos = 'fade-right'>What's</h1>
+                        <h1 className="" data-aos = 'fade-left'>Included</h1>
                     </div>
                     <div className="grid lg:grid-cols-5 gap-11 hover:md:grid-cols-2 hover:sm:grid-cols-1">
                         <div className="" data-aos="zoom-in">
@@ -25,22 +25,22 @@ const Whats = () => {
                             <h1 className="whatsH1">Live Trading <br /> Sessions</h1>
                             <p className="whatsP">Trade live with our team, boasting a combined total of over 7 figures in payouts. Experience live tape reading, real-time trade executions, and ongoing education.</p>    
                         </div>
-                        <div className="backdrop-blur-sm p-5 rounded-lg" data-aos="zoom-in">
+                        <div className="backdrop-blur-sm rounded-lg" data-aos="zoom-in">
                             <span className="iconWhats"><GiBrain/></span>
                             <h1 className="whatsH1">Inclusive Mentorship Programs</h1>
                             <p className="whatsP">Master our trading and account management strategies. Gain an exclusive edge you won't find elsewhere. We guide you step by step through what made us successful.</p>
                         </div>
-                        <div className="backdrop-blur-sm p-5 rounded-lg" data-aos="zoom-in">
+                        <div className="backdrop-blur-sm rounded-lg" data-aos="zoom-in">
                             <span className="iconWhats"><FaHandshake/></span>
                             <h1 className="whatsH1">Supportive <br /> Community</h1>
                             <p className="whatsP">We pride ourselves on having one of the most talented and supportive trading communities. Join the chat and trade alongside fellow traders throughout all trading sessions.</p>
                         </div>
-                        <div className="backdrop-blur-sm p-5 rounded-lg" data-aos="zoom-in">
+                        <div className="backdrop-blur-sm rounded-lg" data-aos="zoom-in">
                             <span className="iconWhats"><IoBarChart/></span>
                             <h1 className="whatsH1">Detailed Chart <br /> Analysis</h1>
                             <p className="whatsP">Access our live market insights, liquidity analysis, and both pre-market and post-market charting.</p>
                         </div>
-                        <div className="backdrop-blur-sm p-5 rounded-lg" data-aos="zoom-in">
+                        <div className="backdrop-blur-sm rounded-lg" data-aos="zoom-in">
                             <span className="iconWhats"><FaMoneyBillTrendUp/></span>
                             <h1 className="whatsH1">Proprietary Firm <br /> Payout Strategies</h1>
                             <p className="whatsP">Our trading team has accumulated over 7 figures in payouts and has more than 30 years of experience. We know what works and we share all our strategies with you.</p>
